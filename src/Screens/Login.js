9c5f32a6c6266842fbc4ddd9f1ccc0dd640354aa
Caio <ca.ragazzi@gmail.com>
2020-05-04 22:11:24 -0300

    UI Login Screen

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,34 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-    SafeAreaView,
+    View,
     StyleSheet,
-    ScrollView,
-    Image
+    TextInput,
+    TouchableOpacity,
+    Text
 } from 'react-native';
 
-import wallpaper from "../Assets/Images/Login/wallpaper.jpg";
-
 const Login = () => {
+
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <Image style={styles.picture} source={wallpaper}>
-        </Image>
+        <View style={styles.container} >
+                <Text style={styles.logo}>Recipes APP</Text>
+            <View style={styles.inputView} >
+                <TextInput
+                    style={styles.inputText}
+                    placeholder="Email..."
+                    placeholderTextColor="#003f5c"
+                    onChangeText={text => setLogin(text)} />
+            </View>
+            <View style={styles.inputView} >
+                <TextInput
+                    style={styles.inputText}
+                    placeholder="Password..."
+                    placeholderTextColor="#003f5c"
+                    secureTextEntry
+                    onChangeText={text => setPassword(text)} />
+            </View>
+            <TouchableOpacity>
+                <Text style={styles.forgot}>Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.loginText}>Signup</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    content: {
-        flex: 1
-    },
-    picture: {
+    container: {
         flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover',
+        backgroundColor: '#003f5c',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        fontWeight: "bold",
+        fontSize: 50,
+        color: "#fb5b5a",
+        marginBottom: 40
+    },
+    inputView: {
+        width: "80%",
+        backgroundColor: "#465881",
+        borderRadius: 25,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20
+    },
+    inputText: {
+        height: 50,
+        color: "white"
+    },
+    forgot: {
+        color: "white",
+        fontSize: 11
+    },
+    loginBtn: {
+        width: "80%",
+        backgroundColor: "#fb5b5a",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10
+    },
+    loginText: {
+        color: "black",
+        fontSize: 11
     },
 })
 
